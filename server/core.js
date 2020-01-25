269b087d859c97b5bcacdff126ca3bf83f4f9445
George Bockari <bitwhys@gmail.com>
2020-01-24 18:35:46 -0800

    feat(core): adds ability to determine winner of a vote and updates state accordingly

--- server/core.js
+++ server/core.js
@@ -2,8 +2,8 @@ import { List, Map } from "immutable"
 
 /**
  *
- * @param state
- * @param entries
+ * @param {Map} state
+ * @param {List | Iterable} entries
  * @return {*}
  */
 export function setEntries(state, entries) {
@@ -12,17 +12,40 @@ export function setEntries(state, entries) {
 
 /**
  *
- * @param state
- * @return {*}
+ * @param {Map} vote An immutable Map of the current vote status
+ * @return {*[]} An array of the element within vote.pair that has
+ * the highest tally, or both if there is a tie
+ */
+// eslint-disable-next-line no-shadow
+function getWinners(vote) {
+  if (!vote) return []
+  const [a, b] = vote.get("pair")
+  const aVotes = vote.getIn(["tally", a], 0)
+  const bVotes = vote.getIn(["tally", b], 0)
+  if (aVotes > bVotes) return [a]
+  if (bVotes > aVotes) return [b]
+  return [a, b]
+}
+
+/**
+ *
+ * @param {Map} state
+ * @return {Map}
  */
 export function next(state) {
-  const entries = state.get("entries")
+  const entries = state.get("entries").concat(getWinners(state.get("vote")))
   return state.merge({
     vote: Map({ pair: entries.take(2) }),
     entries: entries.skip(2),
   })
 }
 
+/**
+ *
+ * @param {Map} state
+ * @param {string} entry
+ * @return {Map}
+ */
 export function vote(state, entry) {
   return state.updateIn(["vote", "tally", entry], 0, tally => tally + 1)
 }
